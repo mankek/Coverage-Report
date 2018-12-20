3fb6e26569324df945ef97d2eb798cfe9eef60f0
mankek <krystal.manke@gmail.com>
2018-12-20 17:12:38 -0600

    added initial report generation

--- Coverage_reports.sh
+++ Coverage_reports.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 usage() {
-        echo "Usage: $0 [-h] [-i BAM FILE]"
+        echo "Usage: $0 [-h] [-i BAM FILE] [-a BED FILE] [-l LIMIT] [-s] [-b]"
         echo "  -h      Displays this help message."
+	echo "	-i	Sets the input bam file."
+	echo "	-a	Sets the input bed file."
+	echo "	-l	Sets the coverage depth limit under which positions are added to the report"
+	echo "	-s	Creates the coverage report using samtools depth for coverage"
+	echo "	-b	Creates the coverage report using bedtools coverage for coverage"
         echo "This is a help message."
         exit
 }
 
-while getopts "hi:s" optchar;
+while getopts "hi:a:l:sb" optchar;
 do
         case $optchar in
                 h)
@@ -14,13 +19,22 @@ do
                         ;;
                 i)
                         bam="$OPTARG"
+			name=${bam:0:(-4)}
                         ;;
                 a)
                         bed="$OPTARG"
                         ;;
+		l)
+			limit="$OPTARG"
+			;;
                 s)
-                        echo ${bam:(-4):4}
+			echo "Chrom	Pos	Cov_depth" > $name"_ST.txt"
+			samtools depth -b $bed $bam | awk -v lim="$limit" '{ if ($3 <= lim) print $0 }' >> $name"_ST.txt"
                         ;;
+		b)
+			echo "Chrom	Region_Start	Region_End	Gene	Pos	Cov_depth" > $name"_BT.txt"
+			bedtools coverage -a $bed -b $bam -d | awk -v lim="$limit" '$6 <= lim' >> $name"_BT.txt"
+			;;
                 \?)
                         echo "Invalid option: -$OPTARG"
                         exit 1
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 Bash script emulating coverage depth report generation of the Coverage tool I made in Python.
+
+Usage:
+
+Once executable permission is given to the file, run it as follows:
+
+	./Coverage_reports -options
+
+Options
+
+-i: Input BAM file
+-a: Input BED file
+-l: Upper limit of coverage depth to include in report
+-s: Use samtools depth to measure coverage depth
+-b: Use bedtoools coverage to measure coverage depth
+-h: Help message
